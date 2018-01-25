fdb820968a51b0b15a8945346e1dc18306396ce5
Borodin Gregory <grihabor@mail.ru>
2018-01-25 11:05:25 +0300

    basic setup + 1 sample

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+NAME = 'rullo'
+
+def get_version():
+    pass
+    
+    
+setup(
+    name=NAME,
+    version=get_version(),
+    packages=['rullo'],
+    install_requires=[
+    
+    ],
+)
