531b45efaa1d8d40417bd4fdf23f994e26660a2d
Borodin Gregory <grihabor@mail.ru>
2018-01-26 16:38:39 +0300

    move checks to separate file

--- rullo/rullo.py
+++ rullo/rullo.py
@@ -1,59 +1,6 @@
 import numpy as np
 import csv
 
-
-def is_line_valid(content,
-                  state,
-                  constraint):
-    """Checks if line is valid
-
-    Attributes
-    ----------
-    content: 1-dim array
-        Line content
-    state: 1-dim array
-        Line state
-
-        state.shape == content.shape
-    constraint: int
-    """
-    return np.sum(content * state) == constraint
-
-
-def is_board_valid(content,
-                   state,
-                   row_constraints,
-                   column_constraints):
-    """Checks if board is valid
-
-    Attributes
-    ----------
-    content: 2-dim array
-        Line content
-    state: 2-dim array
-        Line state
-
-        state.shape == content.shape
-    row_constraints: 1-dim array
-        Array of constraints on rows
-
-        row_constraints.shape[0] == content.shape[0]
-    column_constraints: 1-dim array
-        Array of constraints on columns
-
-        column_constraints.shape[0] == content.shape[1]
-    """
-    assert content.shape == state.shape
-    assert row_constraints.shape[0] == content.shape[0]
-    assert column_constraints.shape[0] == content.shape[1]
-
-    values = content * state
-    return all([
-        np.all(np.sum(values, axis=1) == row_constraints),
-        np.all(np.sum(values, axis=0) == column_constraints),
-    ])
-
-
 class Rullo:
     def __init__(self,
                  content,
