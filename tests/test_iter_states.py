a26d6df696a2ead9cafef83d69a2723cd61799bd
Borodin Gregory <grihabor@mail.ru>
2018-01-26 18:45:50 +0300

    test iter states

---
+++ tests/test_iter_states.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.mark.parametrize('n,expected', [
+    (1, [np.array([0]), 
+         np.array([1])),
+    (2, [np.array([0, 0]), 
+         np.array([0, 1]),
+         np.array([1, 0]),
+         np.array([1, 1])),
+    (3, [np.array([0, 0, 0]), 
+         np.array([0, 0, 1]), 
+         np.array([0, 1, 0]),
+         np.array([0, 1, 1]), 
+         np.array([1, 0, 0]),
+         np.array([1, 0, 1]), 
+         np.array([1, 1, 0]),
+         np.array([1, 1, 1])]),
+])
+def test_iter_states(n, expected):
+    from rullo.solver import iter_states
+    
+    assert list(iter_states(n)) == expected
+    
