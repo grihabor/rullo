54a090a628e2b8f479c6cc585fbe14956711a8c2
Borodin Gregory <grihabor@mail.ru>
2018-01-26 19:45:13 +0300

    fix 3

--- tests/test_iter_valid_states.py
+++ tests/test_iter_valid_states.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 @pytest.mark.parametrize('content,constraint,expected', [
-    (np.array([3,2,5,6,4]), 8, set(np.array([1,0,1,0,0]),
-                                   np.array([0,1,0,1,0]))),
-    (np.array([3,3,2,3]), 6, set(np.array([1,1,0,0]),
-                                 np.array([1,0,0,1]),
-                                 np.array([0,1,0,1]))),
+    (np.array([3,2,5,6,4]), 8, set([np.array([1,0,1,0,0]),
+                                    np.array([0,1,0,1,0])])),
+    (np.array([3,3,2,3]), 6, set([np.array([1,1,0,0]),
+                                  np.array([1,0,0,1]),
+                                  np.array([0,1,0,1])])),
 ])
 def test_iter_valid_states(content, constraint, expected):
     from rullo.solver import iter_valid_states
