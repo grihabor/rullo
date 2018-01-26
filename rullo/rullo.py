36d7408446978ad58776eb14d1a309323f21bc36
Borodin Gregory <grihabor@mail.ru>
2018-01-26 16:37:46 +0300

    remove spaces

--- rullo/rullo.py
+++ rullo/rullo.py
@@ -61,18 +61,18 @@ class Rullo:
                  column_constraints,
                  state=None):
         """Creates a rullo board
-        
+
         Attributes
         ----------
         content: 2-dim array
             Values on the board
         row_constraints: 1-dim array
             Array of constraints on rows
-        
+
             row_constraints.shape[0] == content.shape[0]
         column_constraints: 1-dim array
             Array of constraints on columns
-        
+
             column_constraints.shape[0] == content.shape[1]
         """
         self.content = content
@@ -83,24 +83,19 @@ class Rullo:
             if not shape
             else shape
         )
-    
+
     @classmethod
     def from_csv(cls, f):
         reader = csv.reader(f)
         column_constraints = np.array(next(reader), dtype=np.int)
         row_constraints = np.array(next(reader), dtype=np.int)
-        
+
         content_shape = row_constraints.shape + column_constraints.shape
         content = np.zeros(content_shape, dtype=np.int)
         for i, line in enumerate(reader):
             row = np.array(line, dtype=np.int)
             content[i, :] = row
-        
+
         return cls(content,
                    row_constraints,
                    column_constraints)
-            
-            
-           
-        
-        
