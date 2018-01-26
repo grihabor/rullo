16d96d402107e7e9d2a3acc155d3c19d207ffbd6
Borodin Gregory <grihabor@mail.ru>
2018-01-26 19:07:28 +0300

    fix numpy comparison

--- tests/test_iter_states.py
+++ tests/test_iter_states.py
@@ -3,23 +3,25 @@ import numpy as np
 
 
 @pytest.mark.parametrize('n,expected', [
-    (1, [np.array([0]), 
-         np.array([1])]),
-    (2, [np.array([0, 0]), 
-         np.array([0, 1]),
-         np.array([1, 0]),
-         np.array([1, 1])]),
-    (3, [np.array([0, 0, 0]), 
-         np.array([0, 0, 1]), 
-         np.array([0, 1, 0]),
-         np.array([0, 1, 1]), 
-         np.array([1, 0, 0]),
-         np.array([1, 0, 1]), 
-         np.array([1, 1, 0]),
-         np.array([1, 1, 1])]),
+    (1, np.array([[0], 
+                  [1]])),
+    (2, np.array([[0, 0], 
+                  [0, 1],
+                  [1, 0],
+                  [1, 1]])),
+    (3, np.array([[0, 0, 0], 
+                  [0, 0, 1],
+                  [0, 1, 0],
+                  [0, 1, 1], 
+                  [1, 0, 0],
+                  [1, 0, 1], 
+                  [1, 1, 0],
+                  [1, 1, 1]])),
 ])
 def test_iter_states(n, expected):
     from rullo.solver import iter_states
     
-    assert all(list(iter_states(n)) == expected)
+    assert np.all(
+        np.array(list(iter_states(n))) == expected
+    )
     
