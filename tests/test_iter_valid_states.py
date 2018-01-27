4eb9318f00157aa4e85ec4afce5db21b22b37e60
Borodin Gregory <grihabor@mail.ru>
2018-01-27 08:04:26 +0300

    add more negative tests

--- tests/test_iter_valid_states.py
+++ tests/test_iter_valid_states.py
@@ -5,9 +5,17 @@ import numpy as np
 @pytest.mark.parametrize('content,constraint,valid,expected', [
     (np.array([3,2,5,6,4]), 8, True, [np.array([1,0,1,0,0]),
                                       np.array([0,1,0,1,0])]),
+    (np.array([3,2,5,6,4]), 5, False, [np.array([1,0,1,0,0]),
+                                       np.array([0,1,0,1,0])]),     
+    (np.array([3,2,5,6,4]), 8, False, [np.array([1,0,1,0,0])]),                                                                
     (np.array([3,3,2,3]), 6, True, [np.array([1,1,0,0]),
                                     np.array([1,0,0,1]),
                                     np.array([0,1,0,1])]),
+    (np.array([3,3,2,3]), 6, False, [np.array([1,0,0,1]),
+                                     np.array([0,1,0,1])]),
+    (np.array([3,7,2,3]), 6, False, [np.array([1,1,0,0]),
+                                    np.array([1,0,0,1]),
+                                    np.array([0,1,0,1])]),                                 
 ])
 def test_iter_valid_states(content, constraint, valid, expected):
     from rullo.solver import iter_valid_states
