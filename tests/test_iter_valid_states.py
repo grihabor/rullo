1c6f1fc35ed57ba3840b4f18ec861586dfdf9766
Borodin Gregory <grihabor@mail.ru>
2018-01-27 07:57:15 +0300

    try cast to tuple

--- tests/test_iter_valid_states.py
+++ tests/test_iter_valid_states.py
@@ -3,14 +3,18 @@ import numpy as np
 
 
 @pytest.mark.parametrize('content,constraint,expected', [
-    (np.array([3,2,5,6,4]), 8, set([np.array([1,0,1,0,0]),
-                                    np.array([0,1,0,1,0])])),
-    (np.array([3,3,2,3]), 6, set([np.array([1,1,0,0]),
-                                  np.array([1,0,0,1]),
-                                  np.array([0,1,0,1])])),
+    (np.array([3,2,5,6,4]), 8, [np.array([1,0,1,0,0]),
+                                np.array([0,1,0,1,0])]),
+    (np.array([3,3,2,3]), 6, [np.array([1,1,0,0]),
+                              np.array([1,0,0,1]),
+                              np.array([0,1,0,1])]),
 ])
 def test_iter_valid_states(content, constraint, expected):
     from rullo.solver import iter_valid_states
     
-    assert expected == set(iter_valid_states(content, constraint)) 
+    assert set(map(
+        tuple, expected
+    )) == set(map(
+        tuple, iter_valid_states(content, constraint)
+    ))
     
