0f46d723b9bb5e8a63fee39fab14d3d7df0a62e8
Borodin Gregory <grihabor@mail.ru>
2018-01-28 15:53:32 +0300

    test rullo from csv

---
+++ tests/test_rullo_from_csv.py
@@ -0,0 +1,28 @@
+import pytest
+import numpy as np
+
+
+@pytest.mark.parametrize('content,expected', [
+    (['6,5,9,4',
+      '13,9,10',
+      '4,2,3,3',
+      '5,3,4,3',
+      '1,3,3,2'],
+     [6,5,9,4],
+     [13,9,10],
+     [[4,2,3,3],
+      [5,3,4,3],
+      [1,3,3,2]]),
+])
+def test_rullo_from_csv(content, expected):
+    from rullo.rullo import Rullo
+    
+    f = io.StringIO('\n'.join(content))
+    rullo = Rullo.from_csv(f)
+    
+    assert np.all(rullo.row_constraints == np.array(expected[1]))
+    assert np.all(rullo.column_constraints == np.array(expected[0]))
+    assert np.all(rullo.content == np.array(expected[2]))
+    
+    
+    
