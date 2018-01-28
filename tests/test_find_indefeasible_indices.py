5ad3cf46786191bc54a0d6168742e6a3f4336cbb
Borodin Gregory <grihabor@mail.ru>
2018-01-28 16:35:57 +0300

    test find indefeasable indices

---
+++ tests/test_find_indefeasible_indices.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize('states,expected', [
+    ([[0,1,1,0,0],
+      [1,0,1,0,1],
+      [1,1,1,0,0]], [3, 2]),
+    ([[1,1,0,0,1],
+      [1,0,1,0,1],
+      [1,1,1,0,0]], [0, 3]),
+])
+def test_find_indefeasible_indices(states, expected):
+    from rullo.solver import find_indefeasible_indices
+    
+    states_set = set(map(
+        tuple, states
+    ))
+    
+    assert set(expected) == set(find_indefeasible_indices(state_set))
+    
+    
