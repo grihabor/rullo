befc8c6981aa208379203843274940a156881338
Borodin Gregory <grihabor@mail.ru>
2018-01-30 10:42:04 +0300

    fix intersect test

--- tests/test_intersect.py
+++ tests/test_intersect.py
@@ -2,13 +2,13 @@ import pytest
 
 
 @pytest.mark.parametrize('x,y,expected', [
-    ([(1,), (2,)], 
+    ([(1,), (2,)],
      [(1,3,2), (2,3,3), (3,1,2), (1,1,1), (3,2)],
      [(1,3,2), (2,3,3), (1,1,1)],),
 ])
 def test_intersect(x, y, expected):
     from rullo.intersection import _intersect
 
-    assert expected == _intersect(x, y)
-    
-    
+    assert set(expected) == set(_intersect(x, y))
+
+
