2999ed48cbb640f70cfa5d451ababba67a8d3eb7
Borodin Gregory <grihabor@mail.ru>
2018-01-30 08:49:39 +0300

    move intersection routines to separate file

--- rullo/solver.py
+++ rullo/solver.py
@@ -55,57 +55,6 @@ def _matching_outcome_indices(target_state_set, dependency):
     ] for state in target_state_set]
 
 
-def _have_the_same_beginning(x, y):
-    return any([
-        x == y[:len(x)],
-        y == x[:len(y)],
-    )
-
-
-def _longest(x, y):
-    return (
-        x
-        if len(x) > len(y)
-        else y
-    )
-
-
-def _intersect(outcome_1, outcome_2):
-    product = itertools.product(outcome_1, outcome_2)
-    result = []
-    for item_1, item_2 in product:
-        if _have_the_same_beginning(item_1, item_2):
-            result.append(_longest(*pair))
-
-
-def _outcome_intersection(outcomes):
-
-    result = functools.reduce(
-        _intersect,
-        outcome[1:],
-        outcome[0],
-    )
-    
-    
-    
-
-    if len(outcomes) == 1:
-        outcome = outcomes[0]
-        c = Counter(outcome)
-        new_outcomes = [
-            (
-                outcome_tuple + (index,)
-                if c[outcome
-            )
-            for index, outcome_tuple
-            in outcome
-        ]
-    else:
-        new_outcomes = [
-            
-        ]
-
-    return new_outcomes
 
 
 def _indices_to_outcome(indices, prev_outcome):
