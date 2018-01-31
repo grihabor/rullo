2d5ac3128b3989029eb32302d854b504dff87ea6
Borodin Gregory <grihabor@mail.ru>
2018-01-31 11:21:11 +0300

    test get final outcoms

---
+++ tests/test_get_final_outcomes.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.mark.parametrize('content,row_constraints,column_constraints,expected', [
+    ([
+        [3,3,3,3],
+        [3,3,3,3],
+        [3,3,3,3],
+    ], [
+        9,9,9
+    ], [
+        6,6,6,6
+    ], [
+    
+    ]),
+])
+def test_outcome_intersection(content, row_constraints, column_constraints, expected):
+    from rullo.rullo import Rullo
+    from rullo.solver import get_final_outcomes
+
+    rullo = Rullo(content, row_constraints, column_constraints)
+    assert set(expected) == set(get_final_outcomes(rullo))
+
+
