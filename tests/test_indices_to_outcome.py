5260cccdcbf5043d9d007c3a310a11a2bc079e86
Borodin Gregory <grihabor@mail.ru>
2018-01-31 18:15:59 +0300

    impot outcome

--- tests/test_indices_to_outcome.py
+++ tests/test_indices_to_outcome.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def test_indices_to_outcome():
     from rullo.solver import _indices_to_outcome
-
+    from rullo.outcome import Outcome
+    
     outcome = [(0,), (1,), (2,),]
     indices = [[0], [1, 2], [1, 2]]
     expected = Outcome([
