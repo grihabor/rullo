4d334eb823b413e41698f265783de48377435056
Borodin Gregory <grihabor@mail.ru>
2018-01-31 20:37:38 +0300

    fix test name

--- tests/test_get_final_outcomes.py
+++ tests/test_get_final_outcomes.py
@@ -14,7 +14,7 @@ import pytest
     
     ]),
 ])
-def test_outcome_intersection(content, row_constraints, column_constraints, expected):
+def test_get_final_outcomes(content, row_constraints, column_constraints, expected):
     from rullo.rullo import Rullo
     from rullo.solver import get_final_outcomes
 
